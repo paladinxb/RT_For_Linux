d845412f55979d746cc201f6563c020a5e5151b6
Илья <96050258+paladinxb@users.noreply.github.com>
2023-12-17 19:24:02 +1000

    Update RT.sh

--- RT.sh
+++ RT.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Установка зависимостей Python
 sudo apt-get install python3-pip
-
-# Установка зависимостей Python (если они есть)
+#downloads flet to system
 pip3 install flet
 
 wget https://github.com/paladinxb/RT_For_Linux/blob/main/test.py/
 wget https://github.com/paladinxb/RT_For_Linux/blob/main/interface.py/
-echo "Установка завершена"
+echo "Completed"
 python3 /home/vadim/Загрузки/interface.py
